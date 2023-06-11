54113eecc39ae6173de2bc3dad724b4f18e05940
Gabriel52 <gabrielbrune52@gmail.com>
2023-06-10 23:54:43 -0300

    feat<gym/check-in>: add new tables

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Gabriel Brune',
-    email: 'gabriel@prisma.com',
-  },
-})
 
 export const app = fastify()
